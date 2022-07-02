6e924526c219df82297be6c2736362d12c9ce4fb
Ian Marsman <ian.marsman@gmail.com>
2022-07-01 23:17:50 -0400

    Improved calculations

--- pkg/subnet/subnet.go
+++ pkg/subnet/subnet.go
@@ -3,7 +3,10 @@ package subnet
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"math"
+	"strconv"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 	"inet.af/netaddr"
@@ -14,24 +17,69 @@ const (
 	octetMax = 255
 )
 
-// Subnet an IP subnet
-// https://www.calculator.net/ip-Subnet-calculator.html?cclass=any&csubnet=16&cip=10.0.0.0&ctype=ipv4&printit=0&x=43&y=21
-// https://www.calculator.net/ip-Subnet-calculator.html
-type Subnet struct {
+// IPV4Subnet an IP subnet
+// https://www.calculator.net/ip-IPV4Subnet-calculator.html?cclass=any&csubnet=16&cip=10.0.0.0&ctype=ipv4&printit=0&x=43&y=21
+// https://www.calculator.net/ip-IPV4Subnet-calculator.html
+type IPV4Subnet struct {
 	Prefix *netaddr.IPPrefix `json:"prefix" yaml:"prefix"`
 }
 
-// NewSubnet create new subnet from prefix. Use canonical form if usemasked is true
-func NewSubnet(prefix string, usemasked bool) (subnet *Subnet, err error) {
-	return newSubnet(prefix, usemasked)
+// NewDefaultFromMask parse for 255.255.255.255 starting mask
+func NewDefaultFromMask(mask uint8) (subnet *IPV4Subnet, err error) {
+	return newSubnet("255.255.255.255", mask, true)
 }
 
-func newSubnet(prefix string, usemaked bool) (subnet *Subnet, err error) {
-	subnet = new(Subnet)
+// NewFromMask new subnet with prefix 255.255.255.255 from incoming mask
+func NewFromMask(mask uint8) (subnet *IPV4Subnet, err error) {
+	return newSubnet("255.255.255.255", mask, true)
+}
 
-	pfx, err := netaddr.ParseIPPrefix(prefix)
+// NewDefaultFromPrefix new default (255.255.255.255) from prefix
+func NewDefaultFromPrefix(prefix string) (subnet *IPV4Subnet, err error) {
+	parts := strings.Split(prefix, "/")
+	if len(parts) != 2 {
+		err = errors.New("invalid prefix")
+	}
+	mask, err := strconv.Atoi(parts[1])
 	if err != nil {
 		return
+	}
+	return newSubnet(parts[0], uint8(mask), true)
+}
+
+// NewFromPrefix new using incoming prefix
+func NewFromPrefix(prefix string) (subnet *IPV4Subnet, err error) {
+	parts := strings.Split(prefix, "/")
+	if len(parts) != 2 {
+		err = errors.New("invalid prefix")
+	}
+	mask, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return
+	}
+	return newSubnet(parts[0], uint8(mask), true)
+}
+
+// newSubnet new subnet with prefix string, masked and boolean flag
+func newSubnet(address string, mask uint8, usemasked bool) (subnet *IPV4Subnet, err error) {
+	subnet = new(IPV4Subnet)
+
+	var pfx netaddr.IPPrefix
+	if usemasked {
+		prefixStr := fmt.Sprintf("%s/%d", address, mask)
+		var pfxPre netaddr.IPPrefix
+		pfxPre, err = netaddr.ParseIPPrefix(prefixStr)
+		if err != nil {
+			return
+		}
+		prefixStr = fmt.Sprintf("%s/%d", pfxPre.Masked().IP().String(), mask)
+		pfx, err = netaddr.ParseIPPrefix(pfxPre.Masked().String())
+		if err != nil {
+			return
+		}
+	} else {
+		prefixStr := fmt.Sprintf("%s/%d", address, mask)
+		pfx, err = netaddr.ParseIPPrefix(prefixStr)
 	}
 
 	if !pfx.IsValid() {
@@ -47,6 +95,19 @@ func newSubnet(prefix string, usemaked bool) (subnet *Subnet, err error) {
 	return subnet, nil
 }
 
+func (s *IPV4Subnet) classMask() int {
+	bits := s.Prefix.Masked().IP().As4()
+
+	if s.maxBitsForClass() == 8 {
+		return int(bits[0])
+	} else if s.maxBitsForClass() == 16 {
+		return int(bits[1])
+	} else if s.maxBitsForClass() == 24 {
+		return int(bits[2])
+	}
+	return int(bits[3])
+}
+
 // UsableRange omit first and last IPs
 func UsableRange() (usableRange netaddr.IPRange) {
 
@@ -54,7 +115,7 @@ func UsableRange() (usableRange netaddr.IPRange) {
 }
 
 // JSON get JSON for subnet
-func (s *Subnet) JSON() (bytes []byte, err error) {
+func (s *IPV4Subnet) JSON() (bytes []byte, err error) {
 	bytes, err = json.MarshalIndent(s, "", "  ")
 	if err != nil {
 		return
@@ -64,7 +125,7 @@ func (s *Subnet) JSON() (bytes []byte, err error) {
 }
 
 // YAML get YAML for subnet
-func (s *Subnet) YAML() (bytes []byte, err error) {
+func (s *IPV4Subnet) YAML() (bytes []byte, err error) {
 	bytes, err = yaml.Marshal(s)
 	if err != nil {
 		return
@@ -73,33 +134,67 @@ func (s *Subnet) YAML() (bytes []byte, err error) {
 	return bytes, nil
 }
 
-// ClassBits byte used for subnet
-func (s *Subnet) ClassBits() uint8 {
+// PrefixBits byte used for subnet
+func (s *IPV4Subnet) PrefixBits() uint8 {
 	return s.Prefix.Bits()
 }
 
-// ClassPartialBits bits used in mask block
-func (s *Subnet) ClassPartialBits() uint8 {
-	return s.Prefix.Bits() % 8
+// ClassHostItentifierBits bits not used in mask block
+func (s *IPV4Subnet) ClassHostItentifierBits() uint8 {
+	return s.Prefix.Bits() - s.startBitsForClass()
 }
 
-// UsableHosts number of usable hosts
-func (s *Subnet) UsableHosts() float64 {
-	return math.Exp2(float64(s.ClassPartialBits())) - 2
+// ClassNetworkPrefixBits bits used in mask block
+func (s *IPV4Subnet) ClassNetworkPrefixBits() uint8 {
+	return s.maxBitsForClass() - s.Prefix.Bits()
 }
 
 // Hosts bits remaining in mask block
-func (s *Subnet) Hosts() int64 {
-	return int64(math.Exp2(float64(s.ClassPartialBits())))
+func (s *IPV4Subnet) Hosts() int64 {
+	if s.Prefix.Bits()%8 == 0 {
+		return int64((math.Exp2(float64(32) - float64(s.Prefix.Bits()))) / float64(s.NetworkCount()))
+	}
+	// return int64((math.Exp2(float64(32 - s.Prefix.Bits()))) / float64(s.NetworkCount()))
+	return int64((math.Exp2(float64(32 - s.Prefix.Bits()))))
 }
 
-// ClassUsableHosts how many usable hosts in subnet?
-func (s *Subnet) ClassUsableHosts() int64 {
+// UsableHosts number of usable hosts
+func (s *IPV4Subnet) UsableHosts() int64 {
+	if s.Hosts() < 2 {
+		return 0
+	}
 	return s.Hosts() - 2
 }
 
-// MaxBitsForClass maximum bits for subnet range for the class
-func (s *Subnet) MaxBitsForClass() uint8 {
+// TotalUsableHosts total number of usable hosts
+func (s *IPV4Subnet) TotalUsableHosts() int64 {
+	return s.Hosts() - 2
+}
+
+// // Hosts bits remaining in mask block
+// func (s *Subnet) subBlockBits() int64 {
+// 	return int64(s.Prefix.Bits()) - int64(s.ClassPartialBits())
+// }
+
+// ClassUsableHosts how many usable hosts in subnet?
+func (s *IPV4Subnet) ClassUsableHosts() int64 {
+	return s.Hosts() - 2
+}
+
+// maxBitsForClass maximum bits for subnet range for the class
+func (s *IPV4Subnet) startBitsForClass() uint8 {
+	if s.Prefix.Bits() < 8 {
+		return 0
+	} else if s.Prefix.Bits() < 16 {
+		return 8
+	} else if s.Prefix.Bits() < 24 {
+		return 16
+	}
+	return 24
+}
+
+// maxBitsForClass maximum bits for subnet range for the class
+func (s *IPV4Subnet) maxBitsForClass() uint8 {
 	if s.Prefix.Bits() <= 8 {
 		return 8
 	} else if s.Prefix.Bits() <= 16 {
@@ -110,27 +205,36 @@ func (s *Subnet) MaxBitsForClass() uint8 {
 	return 32
 }
 
-func (s *Subnet) TotalHosts() int64 {
-	return int64(s.BlockSize()) * s.Hosts()
-}
-
 // BlockSize get size of blocks
-func (s *Subnet) BlockSize() uint8 {
-	return uint8(math.Exp2(float64(s.MaxBitsForClass() - s.Prefix.Bits())))
-}
-
-// // SubnetSize number of hosts per subnet
-// func (s *Subnet) SubnetSize() uint8 {
-// 	return 1
+// func (s *Subnet) BlockSize() uint8 {
+// 	// number of subnets is number of bits past block border
+// 	return uint8(math.Exp2(float64(s.maxBitsForClass() - s.Prefix.Bits())))
 // }
 
-// Subnets number of subnets
-func (s *Subnet) Subnets() uint8 {
-	return s.BlockSize()
+// Class get network class, a, b, or c
+func (s *IPV4Subnet) Class() (class rune) {
+	switch s.maxBitsForClass() {
+	case 8:
+		return 'A'
+	case 16:
+		return 'B'
+	case 24:
+		return 'C'
+	default:
+		return '0'
+	}
+}
+
+// NetworkCount number of subnets
+func (s *IPV4Subnet) NetworkCount() int64 {
+	bits := s.Prefix.Bits() - s.startBitsForClass()
+	// bits := s.Prefix.Bits() - 8
+
+	return int64(math.Exp2(float64(bits)))
 }
 
 // UsableIPs get usable ips for subnet
-func (s *Subnet) UsableIPs() (ips []netaddr.IP, err error) {
+func (s *IPV4Subnet) UsableIPs() (ips []netaddr.IP, err error) {
 	ips, err = s.IPs()
 	if err != nil {
 		return
@@ -146,7 +250,7 @@ func (s *Subnet) UsableIPs() (ips []netaddr.IP, err error) {
 }
 
 // IPs get ips for subnet
-func (s *Subnet) IPs() (ips []netaddr.IP, err error) {
+func (s *IPV4Subnet) IPs() (ips []netaddr.IP, err error) {
 	ip := s.Prefix.IP()
 	ips = append(ips, ip)
 
@@ -164,7 +268,7 @@ func (s *Subnet) IPs() (ips []netaddr.IP, err error) {
 }
 
 // Range get subnet range
-func (s *Subnet) Range() (r netaddr.IPRange, err error) {
+func (s *IPV4Subnet) Range() (r netaddr.IPRange, err error) {
 	ip := s.Prefix.IP()
 	startIP := ip
 	for j := 0; j < int(s.Hosts()); j++ {
@@ -180,13 +284,13 @@ func (s *Subnet) Range() (r netaddr.IPRange, err error) {
 	return
 }
 
-// Blocks the set of equally sized subnet blocks for subnet
-func (s *Subnet) Blocks() (r []netaddr.IPRange, err error) {
+// NetworkRanges the set of equally sized subnet blocks for subnet
+func (s *IPV4Subnet) NetworkRanges() (r []netaddr.IPRange, err error) {
 	r = []netaddr.IPRange{}
 	ip := s.Prefix.IP()
 	ipStart := ip
 
-	for j := 0; j < int(s.BlockSize()); j++ {
+	for j := 0; j < int(s.NetworkCount()); j++ {
 		for j := 0; j < int(s.Hosts()); j++ {
 			ip = ip.Next()
 			if (ip == netaddr.IP{}) {
@@ -197,6 +301,50 @@ func (s *Subnet) Blocks() (r []netaddr.IPRange, err error) {
 		}
 		r = append(r, netaddr.IPRangeFrom(ipStart, ip))
 		ipStart = ip.Next()
+	}
+
+	return
+}
+
+func (s *IPV4Subnet) String() string {
+	return s.Prefix.String()
+}
+
+func (s *IPV4Subnet) IncrementPerNetwork() (total int64) {
+	if s.Hosts() == 0 {
+		return 0
+	}
+	total = int64(math.Log(float64(s.Hosts())) / math.Log(2)) // ln(16,384)/ln(2)
+	return total
+}
+
+// Networks get all networks for subnet
+func (s *IPV4Subnet) Networks() (subnets []*IPV4Subnet, err error) {
+	if s.Prefix.Bits() == 1 {
+		err = fmt.Errorf("Can't subdivide")
+		return
+	}
+	// r = []netaddr.IPRange{}
+	ip := s.Prefix.IP()
+	ipStart := ip
+
+	for j := 0; j < int(s.NetworkCount()); j++ {
+		for j := 0; j < int(s.Hosts()); j++ {
+			ip = ip.Next()
+			if (ip == netaddr.IP{}) {
+				err = errors.New("empty ip list in subnet range")
+				subnets = []*IPV4Subnet{}
+				return
+			}
+		}
+		// fmt.Println(ipStart)
+		subnet, err := newSubnet(ipStart.String(), s.Prefix.Bits()-1, false)
+		// fmt.Println(subnet)
+		if err != nil {
+			return []*IPV4Subnet{}, err
+		}
+		ipStart = ip
+		subnets = append(subnets, subnet)
 	}
 
 	return
--- pkg/subnet/subnet_test.go
+++ pkg/subnet/subnet_test.go
@@ -1,41 +1,53 @@
 package subnet
 
 import (
+	"math"
 	"testing"
 
 	"github.com/matryer/is"
+	"inet.af/netaddr"
 )
 
 func TestNewSubnet(t *testing.T) {
+	for i := 1; i <= 32; i++ {
+		// subnetMask := fmt.Sprintf("255.255.255.0/%d", i)
+		is := is.New(t)
+		s, err := NewDefaultFromMask(uint8(i))
+		is.NoErr(err)
+		t.Log("masked", s.Prefix.Masked())
+		t.Log("class mask", s.classMask())
+		t.Log("subnet max bits", s.maxBitsForClass())
+		t.Log("subnet start bits", s.startBitsForClass())
+		// t.Log("block size", s.BlockSize())
+		t.Log("networks", s.NetworkCount())
+		// hosts per network
+		t.Log("count", float64(s.PrefixBits()), int64((math.Exp2(float64(32 - s.Prefix.Bits())))))
+		t.Log("hosts per network", s.Hosts())
+		t.Log("usable hosts per network", s.UsableHosts())
+		t.Log("total per subnet", s.Hosts()*s.NetworkCount())
+		t.Log("class network prefix bits", s.ClassNetworkPrefixBits())
+		t.Log("class host identifier bits", s.ClassHostItentifierBits())
+		t.Log("increment per network", s.IncrementPerNetwork())
+		t.Log()
+	}
+}
+
+func TestNetworks(t *testing.T) {
 	is := is.New(t)
-	s, err := NewSubnet("200.200.200.200/28", true)
+	p, err := netaddr.ParseIPPrefix("192.24.12.0/18")
 	is.NoErr(err)
-	t.Log(s.Prefix)
-	t.Log("subnet max bits", s.MaxBitsForClass())
-	t.Log("block size", s.BlockSize())
-	t.Log("subnets", s.Subnets())
-	t.Log("hosts", s.Hosts())
-	t.Log("total hosts", s.TotalHosts())
-	t.Log("usable hosts", s.UsableHosts())
-
-	r, err := s.Range()
+	t.Log(p.IP())
+	t.Log(p.Masked())
+	s, err := NewFromPrefix(p.Masked().String())
+	networks, err := s.Networks()
 	is.NoErr(err)
-	t.Log("subnet range", r)
-
-	ips, err := s.IPs()
-	t.Log("ips", ips)
-	is.NoErr(err)
-
-	usableIps, err := s.UsableIPs()
-	is.NoErr(err)
-	t.Log("usable ips", usableIps)
-	s.Blocks()
+	t.Log(networks)
 }
 
 // go test -bench=. -benchmem
 func BenchmarkBlocks(b *testing.B) {
 	is := is.New(b)
-	s, err := NewSubnet("200.200.200.200/28", true)
+	s, err := NewDefaultFromMask(28)
 	is.NoErr(err)
-	s.Blocks()
+	s.NetworkRanges()
 }
