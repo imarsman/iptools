e91df1ef3af6c2b8dd49243b2fec08273e65ad05
Ian Marsman <ian.marsman@gmail.com>
2022-09-21 17:04:59 -0400

    Added interface id to output

--- cmd/handler/handler.go
+++ cmd/handler/handler.go
@@ -386,6 +386,7 @@ func subnetIP6Describe(s *ipv6subnet.Subnet) {
 
 	table.Body.Cells = append(table.Body.Cells, row("IP Type", ip6util.AddressType(s.Addr())))
 	table.Body.Cells = append(table.Body.Cells, row("IP", s.Addr().String()))
+	table.Body.Cells = append(table.Body.Cells, row("Interface ID", fmt.Sprintf("%s", s.InterfaceString())))
 	table.Body.Cells = append(table.Body.Cells, row("Prefix", s.Prefix().Masked()))
 	table.Body.Cells = append(table.Body.Cells, row("Routing Prefix", fmt.Sprintf("%s", s.RoutingPrefixString())))
 	table.Body.Cells = append(table.Body.Cells, row("Default Gateway", s.DefaultGatewayString()))
