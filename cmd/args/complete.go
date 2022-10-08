77401af1b70dbf240a0e6c706b3189acc8b8b7a1
Ian Marsman <ian.marsman@gmail.com>
2022-10-08 16:17:02 -0400

    Making arrays local instead of global

--- cmd/args/complete.go
+++ cmd/args/complete.go
@@ -5,14 +5,14 @@ import (
 	"github.com/posener/complete/v2/predict"
 )
 
-// IP6ips list of subnet IP6ips to make life easier
-var IP6ips = []string{"99.236.32.0", "10.32.0.0", "192.168.1.1"}
+// ip4ips list of subnet ip4ips to make life easier
+var ip4ips = []string{"99.236.32.0", "10.32.0.0", "192.168.1.1"}
 
-// IP6PrefixBits number of bits for prefix
-var IP6PrefixBits = []string{"64", "48", "10", "8"}
+// ip6PrefixBits number of bits for prefix
+var ip6PrefixBits = []string{"64", "48", "10", "8"}
 
-// IP6Types IP6 address types
-var IP6Types = []string{
+// ip6Types IP6 address types
+var ip6Types = []string{
 	"global-unicast",
 	"link-local",
 	"private",
@@ -29,7 +29,7 @@ var cmd = &complete.Command{
 				// Scheduler health for an environment
 				"ranges": {
 					Flags: map[string]complete.Predictor{
-						"ip":             predict.Set(IP6ips),
+						"ip":             predict.Set(ip4ips),
 						"bits":           predict.Nothing,
 						"secondary-bits": predict.Nothing,
 						"pretty":         predict.Nothing,
@@ -37,7 +37,7 @@ var cmd = &complete.Command{
 				},
 				"divide": {
 					Flags: map[string]complete.Predictor{
-						"ip":             predict.Set(IP6ips),
+						"ip":             predict.Set(ip4ips),
 						"bits":           predict.Nothing,
 						"secondary-bits": predict.Nothing,
 						"pretty":         predict.Nothing,
@@ -45,7 +45,7 @@ var cmd = &complete.Command{
 				},
 				"describe": {
 					Flags: map[string]complete.Predictor{
-						"ip":             predict.Set(IP6ips),
+						"ip":             predict.Set(ip4ips),
 						"bits":           predict.Nothing,
 						"secondary-bits": predict.Nothing,
 					},
@@ -58,15 +58,15 @@ var cmd = &complete.Command{
 				"describe": {
 					Flags: map[string]complete.Predictor{
 						"ip":     predict.Nothing,
-						"bits":   predict.Set(IP6PrefixBits),
+						"bits":   predict.Set(ip6PrefixBits),
 						"random": predict.Nothing,
-						"type":   predict.Set(IP6Types),
+						"type":   predict.Set(ip6Types),
 					},
 				},
 				"random-ips": {
 					Flags: map[string]complete.Predictor{
 						"number": predict.Nothing,
-						"type":   predict.Set(IP6Types),
+						"type":   predict.Set(ip6Types),
 					},
 				},
 			},
